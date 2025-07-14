2bcb11dfbb37534d46adef369837f4fc06bc595a
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-07-14 10:11:10 -0700

    fix: misc refactoring

--- choose/apps-picker.sh
+++ choose/apps-picker.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 app_list=$(
-  echo "Finder" # bro why can't they just put all the apps in the normal Applications folder
-  ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applications/Utilities/ 3>/dev/null | \
-    grep '\.app$' | sed 's/\.app$//'
+    # bro why can't they just put all the apps in the normal Applications folder
+    echo "Finder"
+    ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applications/Utilities/ 3>/dev/null | \
+        grep '\.app$' | \
+        sed 's/\.app$//'
 )
 
 app_list=$(echo "$app_list" | sort -u)
 
 choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" <<< "$app_list" | \
-  xargs -I {} open -a "{}.app" || exit 0
+    xargs -I {} open -a "{}.app" || exit 0
