249fc51ecf140aeb914ce68eb9717378e57879e6
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-07-14 13:25:56 -0700

    fix: misc refactoring

--- choose/process.sh
+++ choose/process.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-choice=$(printf "kill\nsleep\nreboot\nshutdown" | choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" -p "Select action:")
+choice=$(printf "kill\nsleep\nreboot\nshutdown" | choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" -p "Select action:" -n 5)
 
 case "$choice" in
   kill)
     ps -u "$USER" -o pid=,%cpu=,%mem=,args= | \
       sort -k2 -nr | \
       awk '{pid=$1; cpu=$2; mem=$3; $1=$2=$3=""; printf "%-8s %-6s %-6s %s\n", pid, cpu, mem, $0}' | \
-      choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" -p "Kill process:" -w 35 | \
+      choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" -p "Kill process:" -w 35 -n 20 | \
       awk '{print $1}' | \
       xargs -r kill
     ;;
@@ -15,16 +15,14 @@ case "$choice" in
     osascript -e 'tell application "System Events" to sleep'
     ;;
   reboot)
-    confirm=$(osascript -e 'display dialog "Are you sure you want to reboot?" buttons {"Cancel", "Reboot"} default button "Cancel"')
-    if [[ "$confirm" == *"Reboot"* ]]; then
-      osascript -e 'tell application "System Events" to restart'
-    fi
+    confirm=$(printf "Cancel\nReboot" | \
+      choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" -p "Are you sure?" -n 3)
+    [ "$confirm" = "Reboot" ] && osascript -e 'tell application "System Events" to restart'
     ;;
   shutdown)
-    confirm=$(osascript -e 'display dialog "Are you sure you want to shut down?" buttons {"Cancel", "Shut Down"} default button "Cancel"')
-    if [[ "$confirm" == *"Shut Down"* ]]; then
-      osascript -e 'tell application "System Events" to shut down'
-    fi
+    confirm=$(printf "Cancel\nShut Down" | \
+      choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" -p "Are you sure?" -n 3)
+    [ "$confirm" = "Shut Down" ] && osascript -e 'tell application "System Events" to shut down'
     ;;
   *)
     exit 1
--- choose/wallpapers.sh
+++ choose/wallpapers.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+WALLS="$HOME/Wallpapers"
+
+wallpapers=$(find "$WALLS" -type f | sort)
+
+count=$(echo "$wallpapers" | wc -l)
+
 selection=$(
-  ls "$HOME/Wallpapers" | \
+  ls "$WALLS" | \
     sed 's/\.[^.]*$//' | \
-    choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92"
+    choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" -n $count
 )
 
 # Exit if nothing was selected
 [[ -z "$selection" ]] && exit 1
 
-file_path=$(find "$HOME/Wallpapers" -type f -name "${selection}.*" | head -n 1)
+file_path=$(find "$WALLS" -type f -name "${selection}.*" | head -n 1)
 
 # Exit if no matching file found
 [[ -z "$file_path" ]] && { echo "File not found"; exit 1; }
