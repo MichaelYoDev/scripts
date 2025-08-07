4178a1d2ccc7a3a1f62617368193deefce7f0bc4
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-08-07 14:15:11 -0700

    fix: yabai float

--- stuff/yabai-float.sh
+++ stuff/yabai-float.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-# Get the focused display object
 focused_display=$(yabai -m query --displays | jq '.[] | select(."has-focus" == true)')
-width=$(echo "$focused_display" | jq '.frame.w')
-height=$(echo "$focused_display" | jq '.frame.h')
+width=$(echo "$focused_display" | jq '.frame.w' | cut -d '.' -f 1)
+height=$(echo "$focused_display" | jq '.frame.h' | cut -d '.' -f 1)
 
-# Get number of windows in the currently focused space
 window_count=$(yabai -m query --windows --space | jq 'length')
 
-# Always toggle float
 yabai -m window --toggle float
 
-# Resize if only 1 window & not built-in display
-if [ "$window_count" -eq 1 ] && ! { [ "$width" -eq 1470 ] && [ "$height" -eq 956 ]; }; then
-  yabai -m window --resize left:750:0
-  yabai -m window --resize right:-750:0
+if [ "$window_count" -eq 1 ]; then
+  if [ "$width" -ne 1470 ] || [ "$height" -ne 956 ]; then
+    yabai -m window --resize left:790:0
+    yabai -m window --resize right:-790:0
+  fi
 fi
