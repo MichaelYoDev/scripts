2cfb95d549ec75c29b94cf8bf2a1cfee7cd27dcc
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-09-09 16:57:07 -0700

    fix(notes): add easy change between ghostty and kitty

--- choose/sleep.sh
+++ choose/sleep.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+
+rm /var/folders/*/*/*/com.apple.dock.iconcache
+killall Dock
 osascript -e 'tell app "System Events" to sleep'
--- term/open-term.sh
+++ term/open-term.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-osascript -e "tell application \"Ghostty\" to activate"
+osascript -e "tell application \"kitty\" to activate"
